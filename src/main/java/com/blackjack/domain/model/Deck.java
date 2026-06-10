712f3dbf0a0bd5451d51fdf22c46f78204467979
erictc95 <maxerictc95@hotmail.com>
2026-06-10 15:27:46 +0200

    feat: add deck & hand model

--- src/main/java/com/blackjack/domain/model/Deck.java
+++ src/main/java/com/blackjack/domain/model/Deck.java
@@ -1,4 +1,26 @@
 package com.blackjack.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    private void initializeDeck() {
+        createDeck();
+        shuffleDeck();
+    }
+
+    private void createDeck() {
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+    }
+
+    private void shuffleDeck() {
+
+    }
 }
--- src/main/java/com/blackjack/domain/model/Hand.java
+++ src/main/java/com/blackjack/domain/model/Hand.java
@@ -1,4 +1,45 @@
 package com.blackjack.domain.model;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Getter
 public class Hand {
+
+    private final List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+
+        int score = 0;
+        int aces = 0;
+
+        for (Card card : cards) {
+            score += card.getValue();
+
+            if (card.getRank() == Rank.ACE) {
+                aces++;
+            }
+        }
+
+        while (score > 21 && aces > 0) {
+            score -= 10;
+            aces--;
+        }
+        return score;
+    }
+
+    public boolean isBust() {
+        return calculateScore() > 21;
+    }
+
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
 }
