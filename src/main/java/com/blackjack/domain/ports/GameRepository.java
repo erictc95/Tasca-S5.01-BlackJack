ff18ff570dcb36f0eefa632d6d0e2d7db6457e9c
erictc95 <maxerictc95@hotmail.com>
2026-06-18 17:31:41 +0200

    feat: Game Repository.

---
+++ src/main/java/com/blackjack/domain/ports/GameRepository.java
@@ -0,0 +1,12 @@
+package com.blackjack.domain.ports;
+
+import com.blackjack.domain.model.Game;
+
+import java.util.UUID;
+
+public interface GameRepository {
+
+    void save(Game game);
+
+    Game findById(UUID id);
+}
